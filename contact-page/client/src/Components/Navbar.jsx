58ccfceb5d902e2768238eb3171d4b6e9f77e33c
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-08-09 12:21:30 +0300

    Added a Navbar.jsx file to the components folder

---
+++ contact-page/client/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/contactus">Contact Us</Link>
+          </li>
+          {/* Add more links to other pages if needed */}
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
